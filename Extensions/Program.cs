e0731b523a8f14c9413f2863da360330947d5b42
vhoagamo <vhoagamo@gmail.com>
2021-03-28 00:03:21 +0400

    Add project files.

---
+++ Extensions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var post = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets";
+            while (true)
+            {
+
+                Console.WriteLine("Input number how many words do you want to see...");
+                var input = Console.ReadLine();
+                int number;
+                var num = int.TryParse(input, out number);
+
+                if (!num)
+                {
+                    Console.WriteLine("You entered not valid number... Please try again...");
+                }
+                else
+                {
+                    var result = StringExtensions.Shorten(post, number);
+                    Console.WriteLine(result);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
