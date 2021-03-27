43881070ccfef5ce4f983ebf5889a5a0e557ab78
vhoagamo <vhoagamo@gmail.com>
2021-03-28 02:33:25 +0400

    add EventHandler

--- EventsAndDelegates_03/VideoEncoder.cs
+++ EventsAndDelegates_03/VideoEncoder.cs
@@ -9,12 +9,17 @@ namespace EventsAndDelegates_03
         public VideoEncoder()
         {
         }
+        //There is EventHandler 
+
 
         //1. Define a Delegate which is the contract between the subscriber and publisher, delegate determine the signature of the subscriber
         //2. Define an Event based on delegate
         //3. Raise the published event
         //1
         public delegate void VideoEncodedEventHandler(object source, VideoEventArgs args);
+
+        // We can use instead of custom delegate EventHandler with which additional info we want to send
+        EventHandler<VideoEventArgs>VideoEncoded2;
         //2
         public event VideoEncodedEventHandler VideoEncoded;
         //Event publisher method should be protecetd, virtual and void, should start with word ON and then event Name
