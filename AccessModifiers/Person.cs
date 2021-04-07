b1ae53d9add3ba0ab41c7b494d1b2565c6fac564
hvahagn <vhoagamo@gmail.com>
2021-04-07 15:25:18 +0400

    add changes

---
+++ AccessModifiers/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AccessModifiers
+{
+    public class Person
+    {
+        private DateTime _birthdate;
+        public void SetBirthDate(DateTime birthDate)
+        {
+            _birthdate = birthDate;
+        }
+        public DateTime GetBirtDate()
+        {
+            return _birthdate;
+        }
+        public override string ToString()
+        {
+            return "Hello Vahagn";
+        }
+    }
+}
---
+++ AccessModifiers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.SetBirthDate(new DateTime(1991, 1, 1));
+            var birtDate = person.GetBirtDate();
+            Console.WriteLine(birtDate);
+
+            Console.ReadLine();
+        }
+    }
+}
